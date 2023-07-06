8a96c39830c3b929df05bb73d6916567bf7d0194
yukiharada1228 <yukiharada1228@gmail.com>
2023-07-06 23:41:24 +0900

    gray scale

--- backend/main.py
+++ backend/main.py
@@ -1,24 +1,41 @@
 import tempfile
 
 import cv2
+import numpy as np
 import uvicorn
 from fastapi import FastAPI, File, UploadFile
+from starlette.responses import FileResponse
 
 app = FastAPI()
 
 
 @app.post("/upload")
 async def upload_file(file: UploadFile = File(...)):
-    with tempfile.NamedTemporaryFile() as tmp:
+    # 一時ファイルとして動画保存
+    with tempfile.NamedTemporaryFile(suffix=".mp4", delete=False) as tmp:
         tmp.write(await file.read())
-        filename = tmp.name
-        cap = cv2.VideoCapture(filename)
-        _, frame = cap.read()
-        return {
-            "height": frame.shape[0],
-            "width": frame.shape[1],
-            "channels": frame.shape[2],
-        }
+        input_filename = tmp.name
+
+    # 動画の読み込みと加工
+    cap = cv2.VideoCapture(input_filename)
+    last_frame = None
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        # ここでフレームを加工する処理を行う
+        gray_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        last_frame = gray_img
+
+    cap.release()
+
+    # 加工された最後のフレームを画像ファイルとして保存
+    with tempfile.NamedTemporaryFile(suffix=".jpg", delete=False) as tmp:
+        output_filename = tmp.name
+        cv2.imwrite(output_filename, last_frame)
+
+    # 最後のフレームの画像ファイルをレスポンスとして返す
+    return FileResponse(output_filename, media_type="image/jpeg")
 
 
 if __name__ == "__main__":
