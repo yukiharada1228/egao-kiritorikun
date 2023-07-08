bea677ee288b21080ce1d6f598722dbf6cd5365f
yukiharada1228 <yukiharada1228@gmail.com>
2023-07-08 21:07:25 +0900

    add get

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Button from '@mui/material/Button';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [hello, setHello] = useState([]);
+
+  const handleClick = () => {
+    fetch('http://localhost:8000')
+      .then(res => res.json())
+      .then(data => {
+        setHello(data["smile"]);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>笑顔きりとり君</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <Button variant="contained" onClick={handleClick}>
+          {hello.length ? hello : (<div>笑顔</div>)}
+        </Button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
