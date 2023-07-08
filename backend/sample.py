e05112b64cac04aec644e7c4c79aa50ff1f5e526
yukiharada1228 <yukiharada1228@gmail.com>
2023-07-08 15:00:33 +0900

    best smile

--- backend/sample.py
+++ backend/sample.py
@@ -8,9 +8,10 @@ from openvino.inference_engine import IECore
 from models import EmotionsRecognizer, FaceDetector
 from utils.camera import camera
 
-# ロギングの設定
 logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 logger = logging.getLogger(__name__)
+
+SMILE_INDEX = 1
 PROJECT_ROOT = Path(__file__).resolve().parent
 MODEL_PATH = {}
 FACE_DETECTION_MODEL = "face-detection-retail-0005"
@@ -29,9 +30,12 @@ emotions_recognizer = EmotionsRecognizer(
     ie_core, MODEL_PATH[EMOTIONS_RECOGNITION_MODEL]
 )
 
+best_smile_score = 0
+
 
 @camera
 def process(frame):
+    global best_smile_score
     input_frame = face_detector.prepare_frame(frame)
     infer_result = face_detector.infer(input_frame)
     data_array = face_detector.prepare_data(infer_result, frame)
@@ -42,6 +46,11 @@ def process(frame):
         input_frame = emotions_recognizer.prepare_frame(cropped_frame)
         infer_result = emotions_recognizer.infer(input_frame)
         emotions_score = emotions_recognizer.score(infer_result)
+        smile_score = emotions_score[SMILE_INDEX]
+        is_best = best_smile_score < smile_score
+        best_smile_score = max(best_smile_score, smile_score)
+        if is_best:
+            logger.info({"is_best": is_best, "smile_score": smile_score})
         dest = emotions_recognizer.draw(
             data["xmin"], data["ymin"], data["xmax"], data["ymax"], emotions_score, dest
         )
